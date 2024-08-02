d874913db80dcef8a96f54bc4bff225abbfd88e7
DAFFAsd <76528397+DAFFAsd@users.noreply.github.com>
2024-08-02 13:34:02 +0700

    Add files via upload

--- technoskill-workshop-BE/controllers/manager.controller.js
+++ technoskill-workshop-BE/controllers/manager.controller.js
@@ -1,25 +1,16 @@
 const pg = require("../utils/connect");
 
 exports.register = async function register(req, res) {
-  try {
-    const { email, username, password } = req.body;
-    const response = await pg.query(
-      "INSERT INTO manager (email, username, password) VALUES ($1, $2, $3) RETURNING *",
-      [email, username, password]
-    );
-
-    res.status(201).json(response.rows[0]);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  // Insert kode REGISTER di sini
 };
 
 exports.login = async function login(req, res) {
+  // Insert kode LOGIN di sini
   try {
-    const { email, password } = req.body;
+    const { name, password } = req.body;
     const response = await pg.query(
-      "SELECT * FROM manager WHERE email = $1 AND password = $2",
-      [email, password]
+      "SELECT * FROM manager WHERE name = $1 AND password = $2",
+      [name, password]
     );
     if (response.rows.length === 0) throw new Error("Failed to login");
     res.status(200).json(response.rows);
@@ -27,25 +18,3 @@ exports.login = async function login(req, res) {
     res.status(500).json(error);
   }
 };
-
-exports.getProfile = async function getProfile(req, res) {
-  try {
-    const { email } = req.query; // Retrieve from query parameters
-    if (!email) {
-      return res.status(400).json({ error: "Email is required" });
-    }
-
-    const response = await pg.query(
-      "SELECT email, username, password FROM manager WHERE email = $1",
-      [email]
-    );
-    
-    if (response.rows.length === 0) throw new Error("User not found");
-
-    res.status(200).json(response.rows[0]);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-
